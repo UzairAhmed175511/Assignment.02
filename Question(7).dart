a67b226bc20eb036d0e5d8dae74574fd38eb63a2
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-13 23:09:59 +0500

    chek key and change value

--- Question(7).dart
+++ Question(7).dart
@@ -8,14 +8,12 @@ void main() {
         if exist change it's value to 5000.0 otherwise add 'fri' to expenses
         and set its value to 5000.0 then print expenses.
 */
-
   Map<String, double> expenses = {
     'sun': 3000.0,
     'mon': 3000.0,
     'tue': 3234.0,
     'fri': 3135.0,
   };
-
   if (expenses.containsKey('fri')) {
     expenses['fri'] = 5000.0;
   } else {
