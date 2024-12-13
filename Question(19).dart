629e7b4baba35b98e146e6398eaf1890fe559456
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-14 00:08:18 +0500

    check greater than value in map

--- Question(19).dart
+++ Question(19).dart
@@ -1,7 +1,8 @@
 void main() {
-  //Q.19: Given a map representing a product with keys "name", "price", and "quantity",
-  // write Dart code to check if the product is in stock. If the quantity is greater than 0,
-  // print "In stock", otherwise print "Out of stock".
+  /*Q.19: Given a map representing a product with keys "name", "price", and "quantity",
+          write Dart code to check if the product is in stock. If the quantity is greater than 0,
+          print "In stock", otherwise print "Out of stock".
+  */
   Map product = {"name": "gani", "price": 1200, "quantity": 6};
   if (product["quantity"] > 0) {
     print("In stock");
