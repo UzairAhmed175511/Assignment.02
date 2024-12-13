bd2a257628d7ace7feeeb4869bd995c08cc16ddb
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-14 00:12:21 +0500

    check key and value

--- Question(21).dart
+++ Question(21).dart
@@ -1,7 +1,8 @@
 void main() {
-  //Q.21: Given a map representing a user with keys "name", "isAdmin", and "isActive",
-  // write Dart code to check if the user is an active admin.
-  // If the user is both an admin and active, print "Active admin", otherwise print "Not an active admin".
+  /*Q.21: Given a map representing a user with keys "name", "isAdmin", and "isActive",
+          write Dart code to check if the user is an active admin.
+          If the user is both an admin and active, print "Active admin", otherwise print "Not an active admin".
+  */
   Map user = {"name": "uzair", "isAdmin": true, "isActive": true};
   if (user["isAdmin"] == true && user["isActive"] == true) {
     print("Active admin");
