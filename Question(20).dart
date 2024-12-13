80515585395599a7821f5c0f90008d2008bbce6c
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-14 00:10:26 +0500

    check two condition in map then print

--- Question(20).dart
+++ Question(20).dart
@@ -1,8 +1,8 @@
 void main() {
-  //Q.20: Create a map named "car" with the following key-value pairs: "brand" as "Toyota",
-  // "color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan and red in color.
-  // Print "Match" if both conditions are true, otherwise print "No match".
-
+  /*Q.20: Create a map named "car" with the following key-value pairs: "brand" as "Toyota",
+          "color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan and red in color.
+          Print "Match" if both conditions are true, otherwise print "No match". 
+*/
   Map car = {"brand": "Toyota", "color": "Red", "isSedan": true};
   if (car["isSedan"] == true && car["color"] == "Red") {
     print('Match');
