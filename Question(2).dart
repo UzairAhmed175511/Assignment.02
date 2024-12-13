8198e68328238e7d5a1d16c669d7bffc134ee99e
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-13 22:52:32 +0500

    Names add in list

--- Question(2).dart
+++ Question(2).dart
@@ -1,6 +1,6 @@
 void main() {
   //Q.2: Create an empty list of type string called days.
-  // Use the add method to add names of 7 days and print all days.
+  //     Use the add method to add names of 7 days and print all days.
   List<String> days = [];
   days.add('Monday');
   days.add('Tuesday');
